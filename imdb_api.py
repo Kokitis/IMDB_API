d68ed03b1ccfe2144e56a2d7b25025fb51c15cd3
Kokitis <sypilus@gmail.com>
2017-05-01 23:33:33 -0400

    Added the ability to rename files.

--- imdb_api.py
+++ imdb_api.py
@@ -9,6 +9,8 @@ import json
 import requests
 import matplotlib.pyplot as plt
 import pandas
+import os
+import re
 
 #import timetools as tt
 import scipy.stats as stats
@@ -16,37 +18,56 @@ from pprint import pprint
 from itertools import chain
 #import seaborn as sns
 plt.style.use('fivethirtyeight')
+CACHE_FILENAME = "imdb_cache_file.json"
 
 class IMDBAPI:
 	def __init__(self):
-		pass
-
+		self.cache_file = self._loadCacheFile()
+	def _getFromCache(self, parameters):
+		""" Searches the cache for a specific_value.
+		"""
+		_imdbId = parameters.get('i')
+		_title  = parameters.get('t')
+		key_value = parameters.get(_imdbId, _title)
+		if key_value in self.cache_file:
+			pass
+	def _loadCacheFile(self):
+		if os.path.exists(CACHE_FILENAME):
+			with open(CACHE_FILENAME, 'r') as file1:
+				reader = json.loads(file1.read())
+			return reader
+		else:
+			return dict()
+	def _saveCacheFile(self):
+		with open(CACHE_FILENAME, 'w') as file1:
+			file1.write(json.dumps(self.cache_file, sort_keys = True, indent = 4))
 	def _parseResponse(self, response):
 		response['Response'] = response['Response'] == 'True'
 		return response
 	def _parseSeasons(self, seasons):
-		new_seasons = list()
+		episode_list = list()
 		episode_number = 0
-		for season in seasons:
+		for season_number, season in enumerate(seasons, start = 1):
 			season_num = self._tonum(season['Season'])
 			episodes = season['Episodes']
 			new_episodes = list()
 			for episode in episodes:
 				episode_number += 1
-				episode['Episode'] = self._tonum(episode['Episode'])
-				episode['episodeNumber'] = "S{0:>02}E{1:>02}".format(season_num, episode['Episode'])
-				episode['episodeOrder'] = episode_number
-				episode['imdbRating'] = self._tonum(episode['imdbRating'])
-				new_episodes.append(episode)
-			new_season = {
-				'Episodes': new_episodes,
-				'Response': season['Response'],
-				'Season': self._tonum(season['Season']),
-				'Title': season['Title'],
-				'totalSeasons': self._tonum(season['totalSeasons'])
-			}
-			new_seasons.append(new_season)
-		return new_seasons
+				notation = "S{0:>02}E{1:>02}".format(season_num, episode['Episode'])
+				new_episode = {
+					'imdbId': 		episode['imdbID'],
+					'imdbRating': 	self._tonum(episode['imdbRating']),
+					'index':  		episode_number,
+					'name': 		"{0} - {1}".format(notation, episode['Title']),
+					'notation': 	notation,
+					'number': 		self._tonum(episode['Episode']),
+					'title': 		episode['Title'],
+					'released': 	episode['Released'],
+					'season':		season_number
+				}
+
+				episode_list.append(new_episode)
+		return episode_list
 	@staticmethod
 	def _tonum(value):
 		""" Converts a string to a valid numerical type"""
@@ -176,9 +197,19 @@ class IMDBAPI:
 					***'tomatoUserReviews': string<int>
 						The total number of users that gave the film a rating.
 		"""
+		#self._getFromCache(parameters)
 		endpoint = "http://www.omdbapi.com/"
-		response = requests.get(endpoint, parameters)
-		response = response.json()
+		_expected = '&'.join(["{0}={1}".format(i,j) for i,j in sorted(parameters.items())])
+		if _expected in self.cache_file:
+			response = self.cache_file[_expected]
+		else:
+			response = requests.get(endpoint, parameters)
+			_key_url = response.url.replace(endpoint + '?', '')
+			response = response.json()
+			self.cache_file[_key_url] = response
+			self._saveCacheFile()
+
+
 		response = self._parseResponse(response)
 		return response
 
@@ -187,21 +218,28 @@ class IMDBAPI:
 			Parameters
 			----------
 				title: string
-					The title of a Movie, TV show, or episode. 
+					The title of a Movie, TV show, or episode. imdbIds
+					are also accepted. 
 			Returns
 			----------
 				series : dict<> (from self.by_uid())
 				If the series is not found, returns
 				{'Response': 'False', 'Error': 'Movie not found!'}
 		"""
-		
-		response = self.search(s = title)
-		
-		first_id = response['Search'][0]['imdbID']
+		if title[:2] == 'tt' and any(s.isdigit() for s in title[2:]):
+			first_id = title
+		else:
+			response = self.search(s = title)
+			if 'Search' not in response:
+				print("Warning: In API.find({0}):".format(title))
+				pprint(response)
+			else:
+				first_id = response['Search'][0]['imdbID']
+
 		first_show = self._request(
 			i = first_id,
-			tomatoes = True)
-		first_show['Seasons'] = self.getSeasons(i = first_id)
+			tomatoes = False)
+		first_show['episodeList'] = self.getSeasons(i = first_id)
 
 		return first_show
 
@@ -291,18 +329,28 @@ class IMDBAPI:
 			season_number = 0
 			while True:
 				season_number += 1
-				#url = self.root_url + "i={0}&season={1}".format(i = imdbID, season = season_number)
 				response = self._request(i = imdbID, season = season_number)
 				if 'Error' in response.keys(): break
 				seasons.append(response)
 			seasons = self._parseSeasons(seasons)
 			return seasons
 
-
+	def getEpisodeTitles(self, **kwargs):
+		pass
+	def getImdbId(self, **kwargs):
+		"""  
+		"""
+		pass
+	def getShowStatus(self):
+		""" Determines whether a show is still airing new seasons or 
+			it has completed its run.
+		"""
+		pass
 
 class Parser:
-	""" Accesses the IMDB database via the Open Movie Database API
-		Site: http://www.omdbapi.com/"""
+	""" Parses information saved as a file and outputs relevant 
+		information regarding each saved show.
+	"""
 	def __init__(self, io):
 		""" Parses a file containing show titles/IDs.
 			Parameters
@@ -320,7 +368,7 @@ class Parser:
 				data = json.loads(file1.read())
 			elif ext in {'.csv', 'tsv'}:
 				data = readCSV(filename)
-			elif ext in {'.xls', 'xlsx'}
+			elif ext in {'.xls', 'xlsx'}:
 				data = pandas.read_excel(filename)
 		return data
 
@@ -348,39 +396,35 @@ class GraphTV:
 					*ax:  matplotlib.axes._subplots.AxesSubplot
 						The ax obbject that contains the graph
 		"""
-		_num_episodes = 0
-		#series_info = show.summary()
-		
+		episode_df = pandas.DataFrame(show['episodeList'])
 		if ax is None:
-			colors  = self._generate_colors(response = show, kind = colorscheme)
 			self.fig, self.ax = plt.subplots(figsize = (20,5))
 			self.fig.patch.set_facecolor('#333333')
 		else:
-			colors = self._generate_colors(response = show, kind = 'random')
 			self.fig = None
+		colors = self._generate_colors(
+			bins = episode_df['seasonNumber'].unique(), 
+			kind = colorscheme)
 		self.ax = self._format_ax(self.ax)
 		
-		#Plot the individual seasons
-		
-		#seasons = self._get_seasons()
-		seasons = show['Seasons']
-		self.ax      = self._plot_seasons(self.ax, seasons, colors, 
+
+		self.ax      = self._plot_seasons(self.ax, episode_df, colors, 
 						regression_type = kind, show_dots = show_dots)
 		
-		total_episodes = sum(len(s['Episodes']) for s in seasons)
+		total_episodes = len(episode_df)
 		print("Total Episodes: ", total_episodes)
 		self.ax.set_xlim((0, total_episodes + 1))
 		self.ax.set_ylim(ymax = 10)
 		
 		#return fig, ax
 
-	def _plot_seasons(self, ax, seasons, colors, regression_type = 'linear', show_dots = True):
+	def _plot_seasons(self, ax, episodes, colors, regression_type = 'linear', show_dots = True):
 		""" Plots each season
 			Parameters
 			----------
 				ax: matplotlib.axes._subplots.AxesSubplot
 					The plot to add each series to
-				seasons: list of dicts
+				episodes: pandas.DataFrame
 					A list of every season and episode in the show
 					[[(episode number, rating)]]
 				colors: list
@@ -395,22 +439,21 @@ class GraphTV:
 				ax : matplotlib.axes._subplots.AxesSubplot
 		"""
 		_num_episodes = 0
-		
-		for index, season in enumerate(seasons):
-			season = season['Episodes']
-			season = [(episode['episodeOrder'], episode['imdbRating']) for episode in season]
-			x, y = zip(*season)
-			#print(y)
+		seasons = episodes.groupby(by = 'seasonNumber')
+		for season_number, season_episodes in seasons:
+			x = season_episodes['episodeIndex'].values
+			y = season_episodes['imdbRating'].values
+
 			if show_dots:
-				ax.scatter(x, y, color = colors[index], s = 100)
-			series = [(i, j) for i, j in zip(x, y) if j is not None]
+				ax.scatter(x, y, color = colors[season_number], s = 100)
+			series = list(zip(x, y))
 			if len(series) > 0:
-				rseries = self._calculate_regression(series, kind = regression_type)
-				x, y = zip(*rseries)
-				ax.plot(x, y, color = colors[index])
+				rseries = self._calculateRegression(series, kind = regression_type)
+				rx, ry = zip(*rseries)
+				ax.plot(rx, ry, color = colors[season_number])
 		return ax
 	@staticmethod
-	def _calculate_regression(series, kind = 'average'):
+	def _calculateRegression(series, kind = 'average'):
 		""" Calculates regression lines for the given series
 			Parameters
 			----------
@@ -479,41 +522,180 @@ class GraphTV:
 		ax.yaxis.grid(True)
 		ax.xaxis.grid(False)
 		return ax						
-	def _generate_colors(self, response, kind = 'GraphTV'):
+	def _generate_colors(self, bins, kind = 'GraphTV'):
 		""" Generates a list of colors to use in the graph
 			Parameters
 			----------
+				bins: iterable
+					A list of the bin keys to asign colors to.
 				kind: {'GraphTV', 'random'}; default 'GraphTV'
 					The color map to use
 					*'GraphTV': The series of colors used on the GraphTV webpage
 					*'random': A randomly generated series of colors
+			Returns
+			-------
+				colors: dict<>
 		"""
-		colors = list()
+		colors = dict()
 		if kind == 'GraphTV':
 			colors = ['#79A6F2', '#79F292', '#EE7781', '#C9F279', '#F279ED',
 					'#F9F2D4', '#F2B079', '#8D79F2', '#88F279', '#F279AB',
 					'#79CEF2']
 		else:
-			for season in response['Seasons']:
+			for categ in bins:
 				lower = 100
 				upper = 256
 				red   = random.randrange(lower, upper)
 				blue  = random.randrange(lower, upper)
 				green = random.randrange(lower, upper)
 				color = '#{0:02X}{1:02X}{2:02X}'.format(red, blue, green)
-				colors.append(color)
+				colors[categ] = color
 
 		return colors
 
+class Show:
+	""" Wrapper around the output from the IMDB API. Contains
+		convienience methods for accessing show attributes.
+		Attributes
+		----------
+			show: dict<>
+				Output from the IMDB API.
+	"""
+	def __init__(self, show):
+		"""
+			Parameters
+			----------
+				show: string, dict<>
+					The title or imdbId of a show, or the output from the IMDB API.
+		"""
+		if isinstance(show, str):
+			show = API.find(show)
+		self.show = show
+
+		self.episode_order_regex = re.compile("s(?P<season>[\d]+)e(?P<episode>[\d]+)")
+
+	def __call__(self, *args):
+		""" Returns a specific episode from the season or a metadata value.
+			Parameters
+			----------
+				one argument retrieves a metadata value, if present, or a season
+				notation string of the form SXXEXX. Two areguments designate a
+				season and episode value pair to retrieve an episode.
+		"""
+		if len(args) == 1:
+			value = self.show.get(args[0])
+		else:
+			value = self.selectEpisode(args[0], args[1])
+		return value
+
+	def __str__(self):
+		string = "Show(id = {0}, title = {1})".format(
+			self.show['imdbID'], self.show['Title'])
+		return string
+
+	def selectEpisode(self, season, episode):
+		""" Finds a specific episode in a series.
+			Parameters
+			----------
+				season: int
+				episode: int
+					The episode number. May be either the order of
+					the episode within the season or the overall index.
+			Returns
+			-------
+				episode: dict<>
+		"""
+		for element in self.show['episodeList']:
+			if element['season'] == season:
+				number = element['number']
+				index = element['index']
+				if number == episode or index == episode:
+					return element
+		return None
+
+def _removeIllegalCharacters(string):
+		for char in ['<', '>', ':', '"','/', '\\', '|', '?', '*']:
+			string = string.replace(char, '')
+		return string
+def parseFolder(show, folder, regex = None, rename = False):
+	""" Collects episode names from a folder and retrieves the file names.
+		Parameters
+			show: string
+				The show title or imdbId
+			folder: string
+				path to a folder
+			regex: string
+				A regular expression that produces a valid groupdict
+				with both 'season' end 'episode' groups for each filename.
+				defaults to "(?P<season>s[\d]+)(?P<episode>e[\d]+)"
+			rename: bool; default False
+				Whether to rename the files.
+	"""
+	show = Show(show)
+
+	#template = "{0} S{1:>02}E{2:>02} - {3}"
+	if regex is None:
+		regex = {
+			'KEY01': re.compile("s(?P<season>[\d]+)[\s\.]?e[\s]?(?P<episode>[\d]+)"),
+			'KEY02': re.compile("(?P<season>[\d]+)x(?P<episode>[\d]+)"),
+			'KEY03': re.compile("season[\s](?P<season>[\d]+)[\s]episode[\s](?P<episode>[\d]+)")
+		}
+	else:
+		regex = re.compile(regex)
+	for fn in os.listdir(folder):
+		ext = os.path.splitext(fn)[-1]
+		if isinstance(regex, dict):
+			for k, r in regex.items():
+				match = r.search(fn.lower())
+				if match is not None: break
+		else:
+			match = regex.search(fn.lower())
+		if match is None: continue
+		groups = match.groupdict()
+		season_number = int(groups['season'])
+		episode_number = int(groups['episode'])
+		matched_episode = show(season_number, episode_number)
+		if matched_episode is None:
+			#print("WARNING: Info for {} could not be found!".format(fn))
+			template = "{show} S{s:>02}E{e:>02}"
+			title = ""
+		else:
+			template = "{show} S{s:>02}E{e:>02} - {t}"
+			title = matched_episode['title']
+			title = _removeIllegalCharacters(title)
+		dest = template.format(
+			show = _removeIllegalCharacters(show('Title')), 
+			s = groups['season'], 
+			e = groups['episode'], 
+			t = title)
+		source = os.path.join(folder, fn)
+		destination = os.path.join(folder, dest) + ext
+		print(source)
+		print(destination)
+		if rename:
+			_filename = os.path.join(folder, 'file_rename_log.txt')
+			with open(_filename, 'a') as file1:
+				file1.write(source + ' -> ' + destination + '\n')
+			os.rename(source, destination)
+
+
+API = IMDBAPI()
 #api = IMDB_API()
 
 """Note: The graphTV color scheme only supports up to ~11 seasons, sow shows like supernatural break the program.
 """
 if __name__ == "__main__":
-	api = IMDBAPI()
-	title = 'Blindspot'
-
-	response = api.find(title)
-	#pprint(response)
-	graph = GraphTV(show = response, kind = 'average', colorscheme = 'random')
-	plt.show()
+	"""
+		Add a method that looks up episode titles.
+	"""
+	if False:
+		title = 'tt0367279'
+		print("Searching for", title)
+		response = API.find(title)
+		pprint(response)
+		graph = GraphTV(show = response, kind = 'average', colorscheme = 'random')
+		#plt.show()
+	else:
+		imdbId= 'tt4016454'
+		folder = "D:\\Download Repo\\TV Shows\\Supergirl\\"
+		parseFolder(imdbId, folder, rename = True)
